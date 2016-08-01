10f59fe359c5e97e8a10d7d280109dfc843029a8
MuneshSharma <muneshs16@gmail.com>
2016-08-01 16:53:29 +0530

    https://github.com/MuneshSharma/Context-Menu-On-Right-click-on-Gridview-in-Asp.net.git

---
+++ FacebookLoginMVCApplication/App_Start/AuthConfig.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Web.WebPages.OAuth;
+using FacebookLoginMVCApplication.Models;
+
+namespace FacebookLoginMVCApplication
+{
+    public static class AuthConfig
+    {
+        public static void RegisterAuth()
+        {
+            // To let users of this site log in using their accounts from other sites such as Microsoft, Facebook, and Twitter,
+            // you must update this site. For more information visit http://go.microsoft.com/fwlink/?LinkID=252166
+
+            //OAuthWebSecurity.RegisterMicrosoftClient(
+            //    clientId: "",
+            //    clientSecret: "");
+
+            //OAuthWebSecurity.RegisterTwitterClient(
+            //    consumerKey: "",
+            //    consumerSecret: "");
+
+            OAuthWebSecurity.RegisterFacebookClient(
+                appId: "907552512704446",
+                appSecret: "62f9083a9f410a56f8fd25ba444641a6");
+
+            OAuthWebSecurity.RegisterTwitterClient(
+               consumerKey: "dehAbmGainTnbfVoyrp8jP2OD",
+               consumerSecret: "	EqDT51RO51Lk60RmsNk7VP6gjsmWEQRVvPvvWEODhHpzl8qekr");
+
+            //OAuthWebSecurity.RegisterGoogleClient();
+        }
+    }
+}
---
+++ FacebookLoginMVCApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FacebookLoginMVCApplication.MvcApplication" Language="C#" %>
